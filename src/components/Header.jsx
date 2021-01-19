4aae010b5cacfc8c203a06dadd9d1b9c7dd4326e
To_Do list <jaatishank@gmail.com>
2021-01-19 19:34:43 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AccountCircleIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
